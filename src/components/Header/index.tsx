89b57e2752c108245b9bb752fea13c96a1c12929
acnormun <anaclaranoronha.m@gmail.com>
2023-05-16 20:54:55 -0300

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
-import { HeaderComponent } from "./styles";
+import { Bookmark, DotsThreeOutlineVertical } from "phosphor-react";
+import { HeaderComponent, Icon } from "./styles";
 
-export function Header(){
-    return (
-        <HeaderComponent>
-            alooo
-        </HeaderComponent>
-    )
+export function Header() {
+  return (
+    <HeaderComponent>
+      <Icon>
+        <Bookmark size={32} />
+        LIVROS À JATO
+      </Icon>
+
+      <button>
+        <DotsThreeOutlineVertical size={32} />
+      </button>
+    </HeaderComponent>
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,28 @@
 import styled from "styled-components";
 
 export const HeaderComponent = styled.main`
-    display: flex;
-    heigth: 10vh;
-    background-color: white;
+  display: flex;
+  justify-content: space-between;
+  padding: 1.5rem;
+  heigth: 50vh;
+  width: 100%;
+  background-color: ${(props) => props.theme["gray-700"]};
 
-`
+  button {
+    border: 0;
+    background: none;
+    color: white;
+    cursor: pointer;
+  }
+
+  button::hover{
+    color: ${(props) => props.theme['green-100']}
+  }
+`;
+
+export const Icon = styled.div`
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  gap: 1rem;
+`;
