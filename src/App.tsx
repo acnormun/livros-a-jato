b66b4ea54503c833554132632bcb6161d369abb1
acnormun <anaclaranoronha.m@gmail.com>
2023-05-29 19:38:12 -0300

    api]

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import './App.css'
 import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { SignUp } from './components/Signup'
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
     <Header></Header>
-      <h1>Home</h1>
+      <SignUp/>
       <GlobalStyle/>
     </ThemeProvider>
   )
---
+++ src/components/Signup/index.tsx
@@ -0,0 +1,13 @@
+export function SignUp(){
+
+
+    return(
+        <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email"/>
+
+            <label htmlFor="email">Senha</label>
+            <input type="text" id="email"/>
+        </div>
+    )
+}
